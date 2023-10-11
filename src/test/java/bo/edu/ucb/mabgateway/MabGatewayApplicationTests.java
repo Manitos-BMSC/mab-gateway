44f73741bac660f4992039886f15416d96b2afe3
IsaiDiaz <jsoza057@gmail.com>
2023-10-11 10:31:43 -0400

    First version of gateway with mab-request and mab-user-registry microservices

---
+++ src/test/java/bo/edu/ucb/mabgateway/MabGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.mabgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
